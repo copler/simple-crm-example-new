c0885634d8120121ad5536e5673edb7eb10127f3
Ivan Zhuravel <Ivan_Zhuravel@epam.com>
2016-12-28 23:46:47 +0200

    fix properties reading

--- src/main/java/com/copler/crm/controller/ServerUrlsController.java
+++ src/main/java/com/copler/crm/controller/ServerUrlsController.java
@@ -2,6 +2,7 @@ package com.copler.crm.controller;
 
 import com.copler.crm.dto.Urls;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/resources/ports")
 public class ServerUrlsController {
 
-    @Autowired
-    private Environment env;
+    @Value("${crm.legacy.baseUrl}")
+    private String serverUrl;
+    @Value("${social.bridge.baseUrl}")
+    private String socialUrl;
 
     @RequestMapping(method = RequestMethod.GET)
     public Urls getUrls() {
-        return new Urls(env.getProperty("crm.legacy.baseUrl"),
-                         env.getProperty("social.bridge.baseUrl"));
+        return new Urls(serverUrl, socialUrl);
     }
 }
